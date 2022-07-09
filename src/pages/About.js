8ac490c068d8bc06dfbf07ff28dae834162dafe1
unknown <iftekharaziz932@gmail.com>
2022-07-09 06:37:42 +0600

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex nemo veniam
+        fuga! Provident facilis facere earum iusto doloremque deserunt
+        asperiores nobis, nam sint ipsam impedit iste numquam ea magnam dolor.
+        Quibusdam nisi porro nostrum adipisci totam ab cum nobis dicta.
+      </p>
+    </div>
+  );
+};
+
+export default About;
